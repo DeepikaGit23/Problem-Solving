5fed67af008cfd15070552c1820b274f073e00e5
unknown <dpardeshi5323@gmail.com>
2021-10-27 12:25:53 +0530

    solution correctd

--- src/easy/PasswordDecryption.java
+++ src/easy/PasswordDecryption.java
@@ -12,50 +12,57 @@ public class PasswordDecryption {
 	        {
 	            if(s.charAt(i)>48 &&s.charAt(i)<=57)
 	            {
+	            	System.out.println("Inside number condition char is :"  + s.charAt(i));
 	                stack.push(s.charAt(i));
 	            }
+	            else if(s.charAt(i)=='*')
+	            {
+	            	System.out.println("Inside * condition :" + s.charAt(i));
+	            	res+=" ";
+	            }
+	            else if(s.charAt(i)==48) 
+	        	{
+	            	System.out.println("Inside 0 condition :" + s.charAt(i));
+	            		char popped= stack.pop();
+	            		res+=popped;
+	        		System.out.println("String " + res + "popped char" + popped);
+	        	}
 	            
+//	            else if((s.charAt(i+1)>=97 && s.charAt(i+1)<=122) && (s.charAt(i)>=65 && s.charAt(i)<=90))
+	            else if((i+1)<s.length() && (s.charAt(i)>=65 && s.charAt(i)<=90))
+	        	{
+	            	if(s.charAt(i+1)>=97 && s.charAt(i+1)<=122)
+	            	{
+		            	System.out.println("Swap condition :" + s.charAt(i));
+		        	    char temp='\0';
+		        		char b=s.charAt(i+1);
+		        		char a=s.charAt(i);
+		        		temp=a;
+		        		a=b;
+		        		b=temp;
+		        		res+=a;
+		        		res+=b;
+		        		i++;
+	            	} 
+	            	else
+	            		res+=s.charAt(i);
+	        	}
+	            else {
+	            	res+=s.charAt(i);
+	            }
+	            System.out.println(res);
 	        }
-	 	
-	        int k=0;
-	        while(k<s.length())
-	        {
-	        	char temp='\0';
-	        	if(s.charAt(k)==48) 
-	        	{
-	        		res+=stack.pop();
-	        		System.out.println("K after 2nd if" + k);
-	        		k++;	
-	        	}
-	        	else  if(((s.charAt(k)>48 && s.charAt(k)<=57)) || s.charAt(k)==42)  
-	        	{
-	        		System.out.println("after 1st if" + s.charAt(k));
-	        		k++;
-	        	}
-	        	
-	        	else if(s.charAt(k+1)>=65 && s.charAt(k+1)<=90)
-	        	{
-	        		char b=s.charAt(k+1);
-	        		char a=s.charAt(k);
-	        		temp=a;
-	        		a=b;
-	        		b=temp;
-	        		res+=a;
-	        		res+=b;
-	        		k++;
-	        	}
-	      
-	        	
-	        	
-	        }
+	       res=res.replaceAll(" ","");
 	        return res;
 	    }
 	
 	public static void main(String[] args) {
-		String s="pTo*Ta*0";
+		String s="1pTo*aT*0";
 		String s1="51Pa*0Lp*0e";
-//		potat0  aP1pL5e
+//		poT aT 1  
+//		aP 1pL 5e
 		PasswordDecryption p= new PasswordDecryption();
+		System.out.println(s);
 		System.out.println(p.decryptPassword(s1));
 	}
 }
